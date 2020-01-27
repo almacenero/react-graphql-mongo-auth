61f3622e98a2e22c2765725f78298461abe0ea5d
yasser <yassergeo@gmail.com>
2020-01-27 12:15:23 -0500

    added login form #11

--- frontend/src/Components/Auth/index.js
+++ frontend/src/Components/Auth/index.js
@@ -1,5 +1,16 @@
-import React from "react";
-import { Row, Col, Container } from "reactstrap";
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
+import {
+  Row,
+  Col,
+  Container,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
@@ -7,11 +18,86 @@ const borde = css({
   borderStyle: "solid"
 });
 
+const formStyle = css({
+  borderStyle: "solid",
+  borderWidth: 1,
+  padding: 10,
+  borderRadius: 5
+});
+
+const LOGIN_USER = gql`
+  mutation Login($email: String, $password: String) {
+    login(email: $email, password: $password) {
+      _id
+      email
+      password
+    }
+  }
+`;
+
 const Login = () => {
+  const [createUser, { data }] = useMutation(LOGIN_USER);
+
+  const [emailInput, setemailInput] = useState("");
+  const [passwordInput, setpasswordInput] = useState("");
+
+  const handleChange = e => {
+    const { value, name } = e.target;
+    if (name === "email") {
+      setemailInput(value);
+    }
+    if (name === "password") {
+      setpasswordInput(value);
+    }
+  };
   return (
-    <Row>
-      <Container>Login</Container>
-    </Row>
+    <>
+      <Row>
+        <Container>Login</Container>
+      </Row>
+      <Row>
+        <Container>
+          <Col xs={3}>
+            <Form
+              css={formStyle}
+              onSubmit={e => {
+                e.preventDefault();
+                createUser({
+                  variables: {
+                    email: emailInput,
+                    password: passwordInput
+                  }
+                });
+                setemailInput("");
+                setpasswordInput("");
+              }}
+            >
+              <FormGroup>
+                <Label for="email">Correo Electrónico:</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  value={emailInput}
+                  onChange={handleChange}
+                  bsSize="sm"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="password">Contraseña:</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  value={passwordInput}
+                  onChange={handleChange}
+                  bsSize="sm"
+                />
+              </FormGroup>
+              <Button type="submit">Logear</Button>
+            </Form>
+          </Col>
+        </Container>
+      </Row>
+    </>
   );
 };
 
